995452f8561d0fd274b18a31b726b43c6fd643a1
jean barraez <jeanbarraez@users.noreply.github.com>
2024-01-21 18:43:34 -0300

    se modifico routes

--- routes/joyasRoutes.js
+++ routes/joyasRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 //router.get("/joyas",getAllJoyas );
 //router.get("/joyas_with_limit",getJoyasLimit);
-router.get("/joyas_with_limit_and_order", getOrderAndLimitjoya,);
+router.get("/joyas", getOrderAndLimitjoya,);
 //router.get("/joyas_with_pagination", joyasWithPagination);
 router.get("/joyas_filter",filterControllerjoyas);
 router.get("/joyas_with_hateoas", getControllerjoyasWithHateoas);
